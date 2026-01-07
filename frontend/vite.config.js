57fcc6b32e579ad51c9f5d6a5a33c8865824fb95
Ahn, Ian Tai <ian.tai.ahn@lmco.com>
2026-01-07 12:03:30 -0700

    i don't use the svg anymore, perhaps that is the issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log("Vite config loaded")
 // https://vite.dev/config/
 export default defineConfig({
   base: '/rag/',
